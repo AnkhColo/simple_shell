535117b69666fc7aef916ff121b14f5b0f161021
Colin Muriithi <108725731+AnkhColo@users.noreply.github.com>
2022-12-24 00:29:57 +0300

    Update aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -13,14 +13,12 @@
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 {
 	char *illegal_flag;
-	
 	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
 	_strcat(error, ver_str);
 	_strcat(error, ": ");
 	_strcat(error, datash->args[0]);
 	_strcat(error, msg);
-	
 	if (datash->args[1][0] == '-')
 	{
 
@@ -29,27 +27,21 @@ char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 		illegal_flag[1] = datash->args[1][1];
 		illegal_flag[2] = '\0';
 		_strcat(error, illegal_flag);
-		
 		free(illegal_flag);
 	}
-	
 	else
-		
 	{
 		_strcat(error, datash->args[1]);
 	}
-
 	_strcat(error, "\n");
 	_strcat(error, "\0");
 	return (error);
 }
-
 /**
  * error_get_cd - error message for cd command in get_cd
  * @datash: data relevant (directory)
  * Return: Error message
  */
-
 char *error_get_cd(data_shell *datash
 {
 	int length, len_id;
@@ -66,13 +58,13 @@ char *error_get_cd(data_shell *datash
 		msg = ": can't cd to ";
 		len_id = _strlen(datash->args[1]);
 	}
-
+	
 	length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
 	length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
 	error = malloc(sizeof(char) * (length + 1));
-
+	
 	if (error == 0)
-
+	
 	{
 		free(ver_str);
 		return (NULL);
@@ -94,6 +86,7 @@ char *error_not_found(data_shell *datash)
 	int length;
 	char *error;
 	char *ver_str;
+	
 	ver_str = aux_itoa(datash->counter);
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 
@@ -131,15 +124,12 @@ char *error_not_found(data_shell *datash)
  */
 
 char *error_exit_shell(data_shell *datash)
-
 {
 
 	int length;
 	char *error;
 	char *ver_str;
-
-
-
+	
 	ver_str = aux_itoa(datash->counter);
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 	length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
