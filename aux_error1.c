39aefd55efcbb52485ad94166d3ba9ef39c3b34a
Colin Muriithi <108725731+AnkhColo@users.noreply.github.com>
2022-12-23 15:51:28 +0300

    Update aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -15,6 +15,7 @@
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 {
 	char *illegal_flag;
+	
 	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
 	_strcat(error, ver_str);
@@ -54,6 +55,7 @@ char *error_get_cd(data_shell *datash
 {
 	int length, len_id;
 	char *error, *ver_str, *msg;
+	
 	ver_str = aux_itoa(datash->counter);
 	if (datash->args[1][0] == '-')
 	{
@@ -76,7 +78,6 @@ char *error_get_cd(data_shell *datash
 		free(ver_str);
 		return (NULL);
 	}
-
 	error = strcat_cd(datash, msg, error, ver_str);
 	
 	free(ver_str);
@@ -94,7 +95,6 @@ char *error_not_found(data_shell *datash)
 	int length;
 	char *error;
 	char *ver_str;
-	
 	ver_str = aux_itoa(datash->counter);
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 
@@ -121,7 +121,6 @@ char *error_not_found(data_shell *datash)
 	_strcat(error, ": not found\n");
 	_strcat(error, "\0");
 	free(ver_str);
-	
 	return (error);
 
 }
@@ -146,7 +145,7 @@ char *error_exit_shell(data_shell *datash)
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 	length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
 	error = malloc(sizeof(char) * (length + 1));
-	
+
 	if (error == 0)
 	{
 
