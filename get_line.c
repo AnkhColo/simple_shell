0d3da0365f13e6bf8894971c34dd89a8cc73c5d9
AnkhColo <ankh.collo@gmail.com>
2022-12-26 10:04:46 -0800

    Simple shell

--- get_line.c
+++ get_line.c
@@ -76,7 +76,7 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 		buffer[input] = t;
 		input++;
 	}
-	buffer[input] = '\0'
+	buffer[input] = '\0';
 	bring_line(lineptr, n, buffer, input);
 	retval = input;
 	if (i != 0)
